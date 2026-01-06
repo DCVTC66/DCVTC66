7e77feb429c7bcd73d0ba8100a9af46fb47ea0b5
DCVTC66 <mzeaf@hotmail.fr>
2026-01-06 02:13:33 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dcvtc66-v10"; // ✅ augmente le numéro à chaque changement
+const CACHE_NAME = "dcvtc66-v11"; // ✅ augmente le numéro à chaque changement
 
 const FILES_TO_CACHE = [
   "/",
